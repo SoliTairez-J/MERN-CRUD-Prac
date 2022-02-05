c5598a231224ab16160319b0533753c99fd9673f
Warawit Siriyong <warawit.j@gmail.com>
2022-02-05 17:48:59 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import NavbarComponent from './components/NavbarComponent';
+
+function App() {
+  return (
+    <div className="container p-5">
+      <NavbarComponent/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+//import routes
+const authRoute = require('./routes/auth');
+
+//app
+const app = express();
+
+//connect DB
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true
+})
+    .then(() => console.log('DB CONNECTED'))
+    .catch((err) => console.log('DB CONNECT ERROR', err))
+
+
+//middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
+
+//routes
+app.use('/api', authRoute);
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log('Server is running on port', port));
